eddad96c3874bd01e0d9039bcff4430d27fcfb98
Maleena <maleena.sohail@gmail.com>
2022-08-17 22:17:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Restaurent from './Component/Basics/Restaurent';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='welcome'>Welcome to my restaurent </h1>
+      <Restaurent/>
+
+
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/Component/Basics/menuApi.js
@@ -0,0 +1,75 @@
+const Menu = [{
+    id:1,
+    image:"https://www.bing.com/th?id=OIP.i4Ae6Ev4_qtIextP7uMZIgHaH8&w=241&h=258&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    name:"Maggi",
+    category:"Breakfast",
+    price:25,
+    description:"Easy and light morning breakfast available in all brands",
+},
+{
+    id: 2,
+    image:"https://www.bing.com/th?id=OIP.kl-bJeDS0pza_QFmyIqdzQHaJ4&w=150&h=160&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    name:"aloo pakoda",
+    category:"Dinner",
+    price:50,
+    description:"India very famous and Tasty easy food."
+},
+{
+    id:3 ,
+    image: "https://www.bing.com/th?id=OIP.XITJfQIs4VGEiCCbYGgEuQHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" ,
+    name:  "Biryani",
+    category: "Lunch" ,
+    price: 300 ,
+    description:"Hyderabadi ever green delicious and mouth watering dish"
+},
+
+{
+    id: 4,
+    image: "https://www.bing.com/th?id=OIP.qQ9CCtjHHDqs8o-H3j8XCAHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" ,
+    name: "Pizza" ,
+    category:  "Lunch",
+    price: 250 ,
+    description:"Tasty dish with all the famous Italian toppings"
+},
+{
+    id:5 ,
+    image: "https://www.bing.com/th?id=OIP.PlSH_m8omvBwp4v4xdVOnAHaEK&w=298&h=160&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" ,
+    name:" Aloo puri" ,
+    category: "Dinner" ,
+    price: 150 ,
+    description:"A north Indian dish Famous with Punjabi's and loved By All Indians"
+},
+{
+    id: 6,
+    image: "https://www.bing.com/th?id=OIP.DceJ-mjs2D9tU6fqUmIrUgHaEd&w=322&h=194&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" ,
+    name: "Idli" ,
+    category: "Breakfast" ,
+    price: 75 ,
+    description:"A very famous south Indian dish. soft and Tasty"
+},
+{
+    id: 7,
+    image: "https://www.bing.com/th?id=OIP.jBHGNTQ8TlYMYwB8vqSUzgHaFS&w=295&h=211&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" ,
+    name: "Samosa",
+    category:"Lunch"  ,
+    price: 75 ,
+    description:"famous"
+},
+{
+    id:8 ,
+    image: "https://www.bing.com/th?id=OIP.FH3nQROIfgeC9tIkJ0kWXgHaFP&w=197&h=160&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" ,
+    name: "Rajma Rice" ,
+    category:"Dinner",
+    price: 150 ,
+    description:"Light and Tasty food loved by North Indians"
+},
+{
+    id:9 ,
+    image:"https://www.bing.com/th?id=OIP.eFgNG3E9yK7UR7gJ8hDURQHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+    name: "Poha" ,
+    category: "Breakfast" ,
+    price:90  ,
+    description:"Easy breakfast  loved by All Indians."
+},
+];
+export default Menu;
